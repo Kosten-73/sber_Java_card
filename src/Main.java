f8bbfe11786021fb5a7f89467441ea42b32eefdc
Konstantin Rudenko <korudenko@sfedu.ru>
2024-03-02 18:27:38 +0300

    отдебажил main, корректно все задается

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     }
 
     public static boolean osnova(int day, int month, int year){
-        if (1 <= day && day <= 31 && 1950 <= year && year <= 2300 && 1 <= month && month <= 12) {
+        if (1 <= day && day <= 31 && 1950 <= year && year <= 2010 && 1 <= month && month <= 12) {
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
                 return true;
             } else if (month == 4 || month == 6 || month == 9 || month == 11) {
@@ -67,22 +67,24 @@ public class Main {
         clients[0] = new Client("Руденко Константин Дмитриевич",
                 "korudenko@sfedu.ru",
                 LocalDate.of(2000, 10, 1));
-        for (int i = 1; i < clients.length - 1; i++){
+        for (int i = 1; i < clients.length; i++){
             int randomNumberInRange = 3 + (int)(Math.random() * 20);
-
-            int year = 1950 + (int)(Math.random() * 2010);
+            int year = 1950 + (int)(Math.random() * (2010 - 1950 + 1));
             int mount = 1 + (int)(Math.random() * 12);
             int day = 1 + (int)(Math.random() * 31);
-            System.out.println(day);
+            /*System.out.println(day);
             System.out.println(mount);
             System.out.println(year);
-            System.out.println(!osnova(day, mount, year));
-            while (osnova(day, mount, year)){
+            System.out.println(!osnova(day, mount, year));*/
+            while (!osnova(day, mount, year)){
                 day = 1 + (int)(Math.random() * 31);
             }
             clients[i] = new Client(generateRandomWord(randomNumberInRange),
                     generateRandomEmail(),
                     LocalDate.of(year, mount, day));
+        }
+        for (int i = 1; i < clients.length - 1; i++){
+            System.out.println(clients[i].toString());
         }
     }
 }
